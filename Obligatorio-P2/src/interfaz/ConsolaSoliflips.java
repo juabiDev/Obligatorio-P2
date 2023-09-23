cea73be08da2cf8715c5670c0ac00a8e1853e846
Your Name <you@example.com>
2023-09-23 17:48:07 -0300

    Inicio menu

---
+++ Obligatorio-P2/src/interfaz/ConsolaSoliflips.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaz;
+
+/**
+ *
+ * @author Dell_
+ */
+public class ConsolaSoliflips {
+    public void mostrarMenuPrincipal() {
+        System.out.println("Bienvenido al juego Soliflips!");
+        System.out.println("Seleccione una opción:");
+        System.out.println("a) Cargar datos desde 'datos.txt'");
+        System.out.println("b) Usar el tablero predefinido");
+        System.out.println("c) Generar un tablero aleatorio resoluble");
+        System.out.println("x) Salir del juego");
+        System.out.print("Opción: ");
+    }
+}
--- Obligatorio-P2/src/interfaz/Sistema.java
+++ Obligatorio-P2/src/interfaz/Sistema.java
@@ -2,14 +2,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package grafica;
+package interfaz;
 
+import java.util.*;
 /**
  *
  * @author User
  */
 public class Sistema {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ConsolaSoliflips consola = new ConsolaSoliflips();
+        
+        boolean jugar = true;
+
+        while (jugar) {
+            consola.mostrarMenuPrincipal();
+            String opcion = scanner.nextLine();
+
+            switch (opcion.toLowerCase()) {
+                case "a":
+                    //lógica para cargar datos desde 'datos.txt'
+                    break;
+                case "b":
+                    //lógica para usar el tablero predefinido
+                    break;
+                case "c":
+                    //lógica para generar un tablero aleatorio resoluble
+                    break;
+                case "x":
+                    jugar = false;
+                    System.out.println("¡Gracias por jugar! Hasta luego.");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+                    break;
+            }
+        }
+    
         // Crear un tablero de ejemplo (puedes sustituirlo con tu lógica)
         String[][] tablero = {
             {"/", "\\", "-", "|", "/", "\\", "-", "|", "\\"},
