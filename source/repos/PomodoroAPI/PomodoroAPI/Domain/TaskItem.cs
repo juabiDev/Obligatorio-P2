697e50a9c12ae6208e5737f9f60b6407e48255fe
juabiDev <Mederos40@outlook.com>
2025-01-13 13:10:26 -0300

    feat: create class to represent Tasks

---
+++ source/repos/PomodoroAPI/PomodoroAPI/Domain/TaskItem.cs
@@ -0,0 +1,22 @@
+﻿namespace PomodoroAPI.Domain
+{
+    public class TaskItem
+    {
+        public int TaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+       
+        // Relación con el usuario
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        // Relación con sesiones Pomodoro
+        public ICollection<PomodoroSession> PomodoroSessions { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set;}
+    }
+
+}
