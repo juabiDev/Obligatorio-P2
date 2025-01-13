c6f9ed9b416be4bb6a5feb47287edec465d42436
juabiDev <Mederos40@outlook.com>
2025-01-13 13:12:17 -0300

    feat: add PomodoroSession class

---
+++ source/repos/PomodoroAPI/PomodoroAPI/Domain/PomodoroSession.cs
@@ -0,0 +1,19 @@
+﻿namespace PomodoroAPI.Domain
+{
+    public class PomodoroSession
+    {
+       public int SessionId { get; set; }
+       public DateTime StartTime { get; set; }
+       public DateTime EndTime { get; set; }
+       public int TaskId { get; set; }
+       public TaskItem TaskItem { get; set; }
+       public PomodoroSession() { }
+
+       public PomodoroSession(DateTime startTime, DateTime endTime)
+        {
+           StartTime = startTime;
+           EndTime = endTime;
+        }
+
+    }
+}
