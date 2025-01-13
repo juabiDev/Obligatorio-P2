2ea23ee3e6541899cba14d71a249fe552b9eb422
juabiDev <Mederos40@outlook.com>
2025-01-13 13:12:31 -0300

    feat: add User class

---
+++ source/repos/PomodoroAPI/PomodoroAPI/Domain/User.cs
@@ -0,0 +1,16 @@
+﻿namespace PomodoroAPI.Domain
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        public ICollection<TaskItem> TaskItems { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+
+    }
+}
